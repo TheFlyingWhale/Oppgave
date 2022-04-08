c37973ebdbf131bea043b9a06091e1715f00f6ee
Ole Walberg <ole@studybud.no>
2022-04-08 19:03:41 +0200

    comment landingAnimationHandler.js

--- js/landingAnimationHandler.js
+++ js/landingAnimationHandler.js
@@ -1,6 +1,13 @@
+// Represents the display state
 let displayState = true;
+
+// Contains the landing element
 let landingElement = null;
 
+/** attachAnimationHandler
+ *  Hides the given element on animationend and shows the element before animationstart based on displayState
+ * @param {htmlElement} element 
+ */
 export default function attachAnimationHandler(element){
     landingElement = element;
     
@@ -19,10 +26,16 @@ export default function attachAnimationHandler(element){
     });
 }
 
+/** triggerHideAnimation
+ *  Pretty self explanatory
+ */
 export function triggerHideAnimation(){
     landingElement.style.animation = 'hideLanding 0.35s ease-in-out';
 }
 
+/** triggerShowAnimation
+ *  Pretty self explanatory
+ */
 export function triggerShowAnimation(){
     landingElement.style.animation = 'showLanding 0.35s ease-in-out';
 }
