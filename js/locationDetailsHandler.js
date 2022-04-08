1005e19b0dadce48c15b9fccfca11093e7578793
Ole Walberg <ole@studybud.no>
2022-04-08 18:57:17 +0200

    comment locationDetailsHandler.js

--- js/locationDetailsHandler.js
+++ js/locationDetailsHandler.js
@@ -1,7 +1,15 @@
 import { buildLocationDataDisplay } from './uiBuilder.js';
 
+//Represents the display state
 let displayState = 0;
 
+/** displayDetails
+ *  Displays details for a given location
+ * 
+ * @param {string} address 
+ * @param {integer} avaBikes 
+ * @param {integer} avaParks 
+ */
 export function displayDetails(address, avaBikes, avaParks) {
     const mapOverlay = document.getElementById('mapOverlay');
     mapOverlay.classList.add('mapOverlayTint');
@@ -17,6 +25,9 @@ export function displayDetails(address, avaBikes, avaParks) {
     displayState = 1;
 }
 
+/** hideDetails
+ *  removes the display
+ */
 export function hideDetails() {
     const mapOverlay = document.getElementById('mapOverlay');
     mapOverlay.classList.remove('mapOverlayTint');
